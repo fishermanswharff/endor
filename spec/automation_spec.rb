c813f9b8d0d95e35b088dc5261d921db18679ec4
Jason Wharff <fishermanswharff@mac.com>
2016-06-19 06:35:12 -0400

    gitignore, spec scaffolding

--- spec/automation_spec.rb
+++ spec/automation_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 require_relative '../lib/log_parser'
 
 RSpec.describe LogParser do
-  describe '' do
-    it '' do
+  describe 'Automation' do
+    it 'finds all .log files within a directory, recursively' do
     end
+
+    it 'outputs the contents of every log file, using one shell liner' do
+    end
+
+    it 'outputs lines containing timestamps of form "YYYY-MM-DD HH:MM:SS"'
   end
 end
